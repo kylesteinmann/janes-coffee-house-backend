373c97d61cf2f5cca0e3709925b2a2c22ddb993f
kylesteinmann <105984447+kylesteinmann@users.noreply.github.com>
2023-02-12 14:25:42 -0600

    teat

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,51 +1,40 @@
 class OrdersController < ApplicationController
   before_action :set_order, only: %i[ show update destroy ]
 
-  # GET /orders
-  def index
-    @orders = Order.all
+  # # GET /orders
+  # def index
+  #   @orders = Order.all
 
-    render json: @orders
-  end
+  #   render json: @orders
+  # end
 
-  # GET /orders/1
-  def show
-    render json: @order
-  end
+  # # GET /orders/1
+  # def show
+  #   render json: @order
+  # end
 
   # POST /orders
   def create
     @order = Order.new(order_params)
-    result
-    if @order.save
-      render json: @order, status: :created, location: @order
-    else
-      render json: @order.errors, status: :unprocessable_entity
-    end
+    
+    render Order.result
   end
 
-  # PATCH/PUT /orders/1
-  def update
-    if @order.update(order_params)
-      render json: @order
-    else
-      render json: @order.errors, status: :unprocessable_entity
-    end
-  end
+  #  
 
-  # DELETE /orders/1
-  def destroy
-    @order.destroy
-  end
+  # # DELETE /orders/1
+  # def destroy
+  #   @order.destroy
+  # end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_order
-      @order = Order.find(params[:id])
-    end
+  # private
+  #   # Use callbacks to share common setup or constraints between actions.
+  #   def set_order
+  #     @order = Order.find(params[:id])
+  #   end
 
-    # Only allow a list of trusted parameters through.
-    def order_params
-      params.require(:order).permit(:order)
-    end
+  #   # Only allow a list of trusted parameters through.
+  #   def order_params
+  #     params.require(:order).permit(:order)
+  #   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,42 +1,12 @@
 class Order < ApplicationRecord
     result = client.checkout.create_payment_link(
         body: {
-          idempotency_key: "{UNIQUE_KEY}",
+          idempotency_key: "{123426789}",
           order: {
             location_id: "{LFREXCP0TQ4VT}",
             line_items: [
               {
-                name: "60,000 mile maintenance",
-                quantity: "1",
-                note: "1st line item note",
-                base_price_money: {
-                  amount: 30000,
-                  currency: "USD"
-                }
-              },
-              {
-                name: "Tire rotation and balancing",
-                quantity: "1",
-                base_price_money: {
-                  amount: 15000,
-                  currency: "USD"
-                }
-              },
-              {
-                name: "Wiper fluid replacement",
-                quantity: "1",
-                base_price_money: {
-                  amount: 1900,
-                  currency: "USD"
-                }
-              },
-              {
-                name: "Oil change",
-                quantity: "1",
-                base_price_money: {
-                  amount: 2000,
-                  currency: "USD"
-                }
+                order: @order.order,
               }
             ]
           }
